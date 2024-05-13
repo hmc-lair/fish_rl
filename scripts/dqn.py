37d8740357a1494a11b284c2a93bd075069e4ee9
Chris Herrera <ciherrera20@icloud.com>
2024-05-13 11:06:27 -0700

    Changed dqn.py

--- scripts/dqn.py
+++ scripts/dqn.py
@@ -180,58 +180,6 @@ def optimize_model():
     optimizer.step()
 
 
-if torch.cuda.is_available():
-    print("Cuda is available")
-    num_episodes = 600
-else:
-    print("Cuda is not available")
-    num_episodes = 600
-
-for i_episode in tqdm(range(num_episodes)):
-    # Initialize the environment and get it's state
-    cum_reward = 0
-    state, info = env.reset()
-    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-    for t in count():
-        action = select_action(state)
-        observation, reward, terminated, truncated, _ = env.step(action.item())
-        reward = torch.tensor([reward], device=device)
-        done = terminated or truncated
-
-        if terminated:
-            next_state = None
-        else:
-            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
-
-        # Store the transition in memory
-        memory.push(state, action, next_state, reward)
-
-        # Move to the next state
-        state = next_state
-
-        # Perform one step of the optimization (on the policy network)
-        optimize_model()
-
-        # Soft update of the target network's weights
-        # θ′ ← τ θ + (1 −τ )θ′
-        target_net_state_dict = target_net.state_dict()
-        policy_net_state_dict = policy_net.state_dict()
-        for key in policy_net_state_dict:
-            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
-        target_net.load_state_dict(target_net_state_dict)
-
-        cum_reward += reward
-
-        if done:
-            episode_cum_rewards.append(cum_reward)
-            plot_cum_rewards()
-            break
-
-print('Complete')
-plot_cum_rewards(show_result=True)
-plt.ioff()
-plt.show()
-
 def show_episode():
     env = FlattenObservation(TimeLimit(FishEnv(render_mode="human"), max_episode_steps=1000))
 
@@ -258,3 +206,56 @@ def show_episode():
             print("Cumulative reward:", cum_reward)
             break
     env.close()
+
+if __name__ == "__main__":
+    if torch.cuda.is_available():
+        print("Cuda is available")
+        num_episodes = 600
+    else:
+        print("Cuda is not available")
+        num_episodes = 600
+
+    for i_episode in tqdm(range(num_episodes)):
+        # Initialize the environment and get it's state
+        cum_reward = 0
+        state, info = env.reset()
+        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+        for t in count():
+            action = select_action(state)
+            observation, reward, terminated, truncated, _ = env.step(action.item())
+            reward = torch.tensor([reward], device=device)
+            done = terminated or truncated
+
+            if terminated:
+                next_state = None
+            else:
+                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+
+            # Store the transition in memory
+            memory.push(state, action, next_state, reward)
+
+            # Move to the next state
+            state = next_state
+
+            # Perform one step of the optimization (on the policy network)
+            optimize_model()
+
+            # Soft update of the target network's weights
+            # θ′ ← τ θ + (1 −τ )θ′
+            target_net_state_dict = target_net.state_dict()
+            policy_net_state_dict = policy_net.state_dict()
+            for key in policy_net_state_dict:
+                target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+            target_net.load_state_dict(target_net_state_dict)
+
+            cum_reward += reward
+
+            if done:
+                episode_cum_rewards.append(cum_reward)
+                plot_cum_rewards()
+                break
+
+    print('Complete')
+    plot_cum_rewards(show_result=True)
+    plt.ioff()
+    plt.show()
