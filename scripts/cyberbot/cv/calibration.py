Fixed off by one error in calibration script causing the pixel to meter conversion to be slightly off

scripts/cyberbot/cv/calibration.py
@@ -5,7 +5,7 @@ import argparse
 import yaml
 import sys
 
-def compute_perspective_warp(w, h, rvec, chessboard_size, chessboard_bounds, chessboard_square_size_pixels):
+def compute_perspective_warp(w, h, rvec, chessboard_bounds, chessboard_width_pixels, chessboard_height_pixels):
     '''
     Compute a perspective warp matrix using the calibration parameters so that the chessboard is aligned vertically and horizontally with the frame
     '''
@@ -13,9 +13,9 @@ def compute_perspective_warp(w, h, rvec, chessboard_size, chessboard_bounds, che
     x, y = rotate_matrix.dot(np.hstack([chessboard_bounds, np.ones((len(chessboard_bounds), 1))]).T).T[0]
     outpts = np.float32([
         [0, 0],
-        [chessboard_size[0] * chessboard_square_size_pixels, 0],
-        [chessboard_size[0] * chessboard_square_size_pixels, chessboard_size[1] * chessboard_square_size_pixels],
-        [0, chessboard_size[1] * chessboard_square_size_pixels]
+        [chessboard_width_pixels, 0],
+        [chessboard_width_pixels, chessboard_height_pixels],
+        [0, chessboard_height_pixels]
     ])
     outpts[:, 0] += x
     outpts[:, 1] += y
@@ -81,14 +81,11 @@ def calibrate(port_number, chessboard_size=None, chessboard_square_size_mm=None)
 
     # Code to record clicks
     camera_bounds = np.zeros((2, 2), dtype=int)
-    # num_clicks = 0
     def on_mouse(event, x, y, flags, param):
-        # nonlocal num_clicks
         if event == cv2.EVENT_LBUTTONDOWN:
             camera_bounds[0] = [x, y]
         if event == cv2.EVENT_RBUTTONDOWN:
             camera_bounds[1] = [x, y]
-            # num_clicks += 1
     cv2.setMouseCallback(name, on_mouse)
     
     # Attempt to start video capture
@@ -100,6 +97,7 @@ def calibrate(port_number, chessboard_size=None, chessboard_square_size_mm=None)
     # Try to get the first frame
     if cap.isOpened():
         rval, frame = cap.read()
+        dst = frame.copy()
     else:
         rval = False
 
@@ -127,29 +125,14 @@ def calibrate(port_number, chessboard_size=None, chessboard_square_size_mm=None)
                         objpoints.append(objp)
                         imgpoints.append(corners)
                 else:
-                    if len(objpoints) == 0:
-                        print("error: no chessboard detected", file=sys.stderr)
+                    if len(objpoints) != n_calibration_frames:
+                        print(f"error: no chessboard detected in {n_calibration_frames - len(objpoints)} out of {n_calibration_frames} calibration frames", file=sys.stderr)
                     else:
-                        if len(objpoints) != n_calibration_frames:
-                            print(f"warning: {len(objpoints)}/{n_calibration_frames} calibration frames were dropped", file=sys.stderr)
-                        
                         # Calculate calibration parameters
                         h, w = frame.shape[:2]
                         _, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, (w, h), None, None)
                         rvec = np.average(rvecs, axis=0)
                         tvec = np.average(tvecs, axis=0)
-
-                        # Compute the bounds on the chessboard and the pixel size of a chessboard square
-                        chessboard_bounds = np.zeros((len(imgpoints), 4, 2))
-                        for i, imgp in enumerate(imgpoints):
-                            chessboard_bounds[i] = corners.reshape(-1, 2)[[0, chessboard_size[0]-1, -1, -chessboard_size[0]]]
-                        chessboard_bounds = np.average(chessboard_bounds, axis=0)
-                        chessboard_width_pixels = np.linalg.norm(chessboard_bounds[1] - chessboard_bounds[0]) / chessboard_size[0]
-                        chessboard_height_pixels = np.linalg.norm(chessboard_bounds[3] - chessboard_bounds[0]) / chessboard_size[1]
-                        chessboard_square_size_pixels = np.average([chessboard_width_pixels, chessboard_height_pixels])
-
-                        # Compute a perspective warp so that the chessboard is aligned vertically and horizontally
-                        perspective_warp = compute_perspective_warp(frame.shape[1], frame.shape[0], rvec, chessboard_size, chessboard_bounds, chessboard_square_size_pixels)
 
                         # Compute the re-projection error. Closer to 0 is better
                         mean_error = 0
@@ -158,22 +141,35 @@ def calibrate(port_number, chessboard_size=None, chessboard_square_size_mm=None)
                             error = cv2.norm(imgp, reprojected_imgp, cv2.NORM_L2)
                             mean_error += error / len(objpoints)
                         print("re-projection error: {}".format(error), file=sys.stderr)
+
+                        # Re-find chessboard corners after undistorting
+                        dst2 = cv2.undistort(frame, mtx, dist)
+                        _, undistorted_corners = cv2.findChessboardCorners(dst2, chessboard_size, flags=flags)
+
+                        # Compute the bounds on the chessboard and the pixel size of a chessboard square
+                        chessboard_bounds = undistorted_corners.reshape(-1, 2)[[0, chessboard_size[0]-1, -1, -chessboard_size[0]]]
+                        chessboard_width_pixels = np.linalg.norm(chessboard_bounds[1] - chessboard_bounds[0]) / (chessboard_size[0] - 1)
+                        chessboard_height_pixels = np.linalg.norm(chessboard_bounds[3] - chessboard_bounds[0]) / (chessboard_size[1] - 1)
+                        chessboard_square_size_pixels = np.average([chessboard_width_pixels, chessboard_height_pixels])
+
+                        # Compute a perspective warp so that the chessboard is aligned vertically and horizontally
+                        perspective_warp = compute_perspective_warp(frame.shape[1], frame.shape[0], rvec, chessboard_bounds, chessboard_width_pixels, chessboard_height_pixels)
                 calibration_frames -= 1
 
             # Rendering
-            dst = frame
             if mtx is not None and dist is not None and perspective_warp is not None:
-                dst = cv2.undistort(frame, mtx, dist)
+                dst = cv2.undistort(dst, mtx, dist)
                 dst = cv2.warpPerspective(dst, perspective_warp, (frame.shape[1], frame.shape[0]))
             dst = cv2.rectangle(dst, camera_bounds[0], camera_bounds[1], (255, 0, 0), 3)
             cv2.imshow(name, dst)
 
             # Read a new frame
             rval, frame = cap.read()
+            dst = frame.copy()
 
             # Try to detect a chessboard
             chessboard_found, corners = cv2.findChessboardCorners(frame, chessboard_size, flags=flags)
-            cv2.drawChessboardCorners(frame, chessboard_size, corners, chessboard_found)
+            cv2.drawChessboardCorners(dst, chessboard_size, corners, chessboard_found)
 
             # Keypress handling
             key = cv2.waitKey(1)
@@ -203,6 +199,7 @@ def calibrate(port_number, chessboard_size=None, chessboard_square_size_mm=None)
             "CHESSBOARD_BOUNDS": chessboard_bounds.tolist(),
             "CHESSBOARD_SQUARE_SIZE_PIXELS": chessboard_square_size_pixels.tolist(),
             "CHESSBOARD_SQUARE_SIZE_MM": chessboard_square_size_mm,
+            "METERS_PER_PIXEL": (chessboard_square_size_mm / 1000 / chessboard_square_size_pixels).tolist(),
             "CAMERA_BOUNDS": camera_bounds.tolist()
         }
     else:

scripts/cyberbot/cv/camera.py
@@ -1,27 +1,17 @@
 
 import numpy as np
 import cv2
-import os
-from datetime import datetime
-from cv import convert
-
-PIX2METERS = .635/820 # meters/pixels conversion TODO: automate this calculation in __init__
-FPS = 10
-
-# MTX and DIST are properties of the camera (have to do with fisheye lens)
-MTX = np.array([[1.05663779e+03, 0.00000000e+00, 9.73055094e+02],
- [0.00000000e+00, 1.05269643e+03, 5.64799418e+02],
- [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
-DIST = np.array([-3.80359934e-01,  1.49531854e-01,  2.50649988e-05,  8.39488578e-05,  -2.83529982e-02])
+from cv.calibration import load_calibration_params, apply_calibration
 
 class VideoProcessor:
     """Class to handle video processing, displaying, CV, etc."""
 
-    def __init__(self, camera_port, camera_bounds):
+    def __init__(self, camera_port, calibration_path):
+        self._calibration_params = load_calibration_params(calibration_path)
         self._cap = cv2.VideoCapture(camera_port)
-        self._height = camera_bounds[1,1]-camera_bounds[0,1]
+        self._bounds = self._calibration_params["CAMERA_BOUNDS"]
+        self._height = self._bounds[1,1] - self._bounds[0,1]
         self._current_frame = None
-        self._bounds = camera_bounds
 
     def get_coords(self, num_objects):
         """Finds the n largest dark objects and returns their centroids in order"""
@@ -29,18 +19,17 @@ class VideoProcessor:
         coords = np.zeros([num_objects, 2])
         ret, frame = self._cap.read()
 
-        frame = cv2.undistort(frame, MTX, DIST, None, MTX)
-        frame = frame[self._bounds[0][1]:self._bounds[1][1], self._bounds[0][0]:self._bounds[1][0]]
+        frame = apply_calibration(frame, self._calibration_params)
         self._current_frame = frame.copy()
 
         if (ret is None or frame is None): return coords # if frame isn't valid, return
 
         ## Orange thresholding for the robot to follow the orange dots
 
-        lure_hsv =cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        lure_hsv =cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
  
-        lower_blue = np.array([100,170,50], dtype = "uint8")  # 100, 50, 0
-        upper_blue = np.array([120,255,255], dtype = "uint8")  # 140, 255, 255
+        lower_blue = np.array([100,50,120], dtype = "uint8")  # 100, 50, 0
+        upper_blue = np.array([140,255,255], dtype = "uint8")  # 140, 255, 255
 
         lure_mask=cv2.inRange(lure_hsv,lower_blue,upper_blue)
         kernellure = np.ones((10,10),np.uint8)
@@ -64,13 +53,12 @@ class VideoProcessor:
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
 
-                met_cX = convert.xpxtomet(cX)
-                met_cY = convert.ypxtomet(self._height - cY)
+                met_cX = cX * self._calibration_params['METERS_PER_PIXEL']
+                met_cY = (self._height - cY) * self._calibration_params['METERS_PER_PIXEL']
 
             else: cX, cY = 0, 0
             cv2.circle(self._current_frame, (cX, cY), int(5/(i+1)), (320, 159, 22), -1)
             coords[i,:] = np.array([met_cX, met_cY])
-        # coords[:,1] = (convert.ypxtomet(self._height)) - coords[:,1] # move origin to lower left corner
         return coords
     
     def get_robot_state(self):
@@ -85,8 +73,8 @@ class VideoProcessor:
         """Shows live video feed, plotting dots on identified objects and the bot target"""
 
         if self._current_frame is not None:
-            xpx = convert.xmettopx(target.x)
-            ypx = convert.ymettopx(target.y)
+            xpx = target.x / self._calibration_params["METERS_PER_PIXEL"]
+            ypx = target.y / self._calibration_params["METERS_PER_PIXEL"]
             cv2.circle(self._current_frame, (int(xpx), int(self._height-ypx)), 5, (0, 159, 22), -1)
             cv2.namedWindow("output", cv2.WINDOW_NORMAL)
             resized = cv2.resize(self._current_frame, (960, 540))

scripts/cyberbot/testrun.py
@@ -7,9 +7,9 @@ from simple_pid import PID
 import math
 
 class RLController():
-    def __init__(self, camera_port=0, camera_bounds = np.array([[636,  302], [1500, 782]]), transmit_port='/dev/tty.usbmodem14102'):
+    def __init__(self, transmit_port, camera_port, calibration_path):
         self._ser = serial.Serial(transmit_port, baudrate=115200)
-        self._video = camera.VideoProcessor(camera_port, camera_bounds)
+        self._video = camera.VideoProcessor(camera_port, calibration_path)
 
     def send_commands(self, vR, vL):
        """Sends commands to the transmit board over the serial port"""
@@ -50,21 +50,24 @@ def wrap_to_pi(x):
 
 if __name__ == '__main__':
     # Setup serial communication to robot and the video processing code
-    camera_bounds = np.array([[636,  302], [1500, 782]])  # find these with calibrate_setup.py
-    port_t = '/dev/ttyACM0'                     # find this with ls /dev | grep usbmodem
-    port_c = 2                                            # either 0 or 1
-    c = RLController(camera_bounds=camera_bounds, camera_port=port_c, transmit_port=port_t)
+    c = RLController('/dev/ttyACM0', 0, 'cv/calibration.yaml')
 
     # Trajectory for the robot to follow
+    # points = np.array([
+    #     [0.04, 0.26],
+    #     [.1, .14],
+    #     [0.32, 0.12],
+    #     [.55, .13],
+    #     [0.66, 0.22],
+    #     [.58, .36],
+    #     [0.38, 0.34],
+    #     [.13, .35]
+    # ])
     points = np.array([
-        [0.04, 0.26],
-        [.1, .14],
-        [0.32, 0.12],
-        [.55, .13],
-        [0.66, 0.22],
-        [.58, .36],
-        [0.38, 0.34],
-        [.13, .35]
+        [0.40, 0.14],
+        [0.72, 0.25],
+        [0.44, 0.43],
+        [0.09, 0.29]
     ])
     current_point_idx = 0
     current_robot_state = (0, 0, 0)
@@ -134,5 +137,5 @@ if __name__ == '__main__':
         commanded_vels = np.array([0, 0], dtype=np.float64)
         current_robot_state = c.get_video_state()
         sm.run()
-        # c.send_commands(*commanded_vels)
+        c.send_commands(*commanded_vels)
         time.sleep(0.005)  # Highest possible command frequency is 0.0032 (312.5 commands per second)